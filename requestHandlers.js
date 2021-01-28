b3595bd714a5fbc4d7403a8439a0fda654cc9ecc
Artyom V. Poptsov <avp@nntc.nnov.ru>
2021-01-28 15:49:48 +0300

    Добавлен ответ на запрос списка на основе данных из БД

--- requestHandlers.js
+++ requestHandlers.js
@@ -6,14 +6,17 @@ let sequelize = new Sequelize('db', 'user', '123456',
 class Item extends Model {};
 
 Item.init({
-	id: {
-	    type: DataTypes.UUID,
-	    defaultValue: DataTypes.UUIDV1,
-	    primaryKey: true
-  	},
-  	title: DataTypes.STRING,
-  	filepath: DataTypes.STRING
-	}, { sequelize, modelName: 'items' });
+		id: {
+	    	type: DataTypes.UUID,
+	    	defaultValue: DataTypes.UUIDV1,
+	    	primaryKey: true
+  		},
+  		title: DataTypes.STRING,
+  		filepath: DataTypes.STRING
+	}, { 
+		sequelize, 
+		modelName: 'items' 
+});
 
 Item.sync(); // { force: true }
 
@@ -30,36 +33,18 @@ function getList(request, response){
 		return;
 	}*/
 
-	Item.findAll({}).then(function(data){
-		console.log(`data? ${data}`);
+	Item.findAll({}).then(function(data) {
+		var result = [];
+		data.forEach(function (e) {
+			result.push({ id: e.id, title: e.title, filepath: e.filepath });
+		});
+		var dataJSON = JSON.stringify(result);
+		response.writeHead(200, { "Content-Type": "application/json" });
+		response.write(dataJSON);
+		response.end();
 	}).catch(function(err){
 		console.log(`err? ${err}`);
 	})
-
-	var data = [
-		{id:1, title:"Рис. 1"},
-		{id:2, title:"Рис. 2"},
-		{id:4, title:"Рис. 4"},
-		{id:1, title:"Рис. 1"},
-		{id:2, title:"Рис. 2"},
-		{id:4, title:"Рис. 4"},
-		{id:1, title:"Рис. 1"},
-		{id:2, title:"Рис. 2"},
-		{id:4, title:"Рис. 4"},
-		{id:1, title:"Рис. 1"},
-		{id:2, title:"Рис. 2"},
-		{id:4, title:"Рис. 4"},
-		{id:5, title:"Рис. 5"}
-	];
-
-	var dataJSON = JSON.stringify(data); // JSON.parse(dataJSON)
-	response.writeHead(200, {"Content-Type": "application/json"});
-	response.write(dataJSON);
-
-	setTimeout(function(){
-		response.end();
-	}, 3000);
-	
 }
 
 function create(request, response){
--- router.js
+++ router.js
@@ -2,7 +2,8 @@ var nodeStatic = require('node-static');
 var fileServer = new nodeStatic.Server(`${__dirname}/static`);
 
 function route(handle, pathname, request, response) {
-  console.log("About to route a request for " + pathname);
+	//onsole.log("Request:", request);
+    console.log(`About to route a request from ${request.socket.remoteAddress} for ${pathname}`);
   if ((typeof handle[pathname]) === 'function') {
     handle[pathname](request, response);
   } else {

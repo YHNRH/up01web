9ef018416143fafefeb9823fde91f76f9b836e56
Vladimir G. Slugin <v.slugin@nntc.nnov.ru>
2021-02-01 13:17:06 +0300

    пример переключения между страницамми одностраничного приложения.
    Стр. 17-41 файла static/js/frontend.js

--- static/js/frontend.js
+++ static/js/frontend.js
@@ -1,7 +1,45 @@
+var intervalPointer;
 
 function router(state){
-	console.log('REOUTER WAS CALLED. HASH=', document.location.hash);
-	test();
+	var route = document.location.hash.replace('#', ''); // login, list, e.t.c...
+
+	switch(route){
+		case 'login':
+		document.getElementById("app").innerHTML='Здесь будет форма логина';
+		break;
+		case 'list':
+		document.getElementById("app").innerHTML='Здесь будет форма списка';
+		break;
+		default:
+		document.getElementById("app").innerHTML='Здесь будет форма списка';
+	}
+
+	/**
+	 * пример переключения между страницамми одностраничного приложения (без перезагрузки страницы)
+	 **/
+
+	// очистка таймаута (функция router -  это коллбэк на события popstate и load объекта window)
+	// поэтому необходимо при каждом запуске этой функции отменять предыдущий setInterval,
+	// иначе коллбэки на setInterval будут копиться и приложение будет работать неправильно
+	clearInterval(intervalPointer);
+
+	// запуск функции setInterval с сохранением указателя на неё в переменной выше области видимости
+	// функции router (для отмены и переназначения при следующем вызове)
+	intervalPointer = setInterval(function(){
+		// раз в секунду переключаемся между маршрутами login и list
+		var newRoute;
+		if(route === 'login'){
+			newRoute = 'list';
+		} else if(route === 'list'){
+			newRoute = 'login';
+		} else {
+			newRoute = 'list';
+		}
+
+		// непосредственно переключение
+		document.location.hash = `#${newRoute}`;
+	}, 1000); // раз в секунду
+
 }
 
 
